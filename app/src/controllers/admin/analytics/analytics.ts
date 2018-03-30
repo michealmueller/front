feab99e3990db4d1ea8e53637600d97540c1ec5e
Micheal Mueller <arthmael86@gmail.com>
2018-03-30 09:48:52 -0400

    some changes from new version

--- app/src/controllers/admin/analytics/analytics.ts
+++ app/src/controllers/admin/analytics/analytics.ts
@@ -4,85 +4,99 @@ import { Location } from '@angular/common';
 import { Client, Upload } from '../../../services/api';
 
 @Component({
-  moduleId: module.id,
-  selector: 'minds-admin-analytics',
-  templateUrl: 'analytics.html'
+    moduleId: module.id,
+    selector: 'minds-admin-analytics',
+    templateUrl: 'analytics.html'
 })
 
 export class AdminAnalytics {
 
-  dam;
-  dam_list;
-  mam;
-  mam_list;
-  signups;
-  signups_list;
-  retention;
-  boost_newsfeed = {
-    review: 0,
-    approved: 0,
-    percent: 50,
-    total: 0,
-    review_backlog: 0,
-    approved_backlog: 0,
-    impressions: 0,
-    impressions_met: 0
-  };
+    dam;
+    dam_list;
+    mam;
+    mam_list;
+    signups;
+    signups_list;
+    retention;
+    boost_newsfeed = {
+        review: 0,
+        approved: 0,
+        percent: 50,
+        total: 0,
+        review_backlog: 0,
+        approved_backlog: 0,
+        impressions: 0,
+        impressions_met: 0
+    };
+    pageviews;
+    pageviews_list;
 
-  constructor(public client: Client) {
-    this.getActives();
-    this.getSignups();
-    this.getRetention();
-    this.getBoosts();
-  }
+    constructor(public client: Client) {
+        this.getActives();
+        this.getPageviews();
+        this.getSignups();
+        this.getRetention();
+        this.getBoosts();
+    }
 
-  /**
-   * Return active user analytics
-   */
-  getActives() {
-    var self = this;
-    this.client.get('api/v1/admin/analytics/active')
-      .then((response: any) => {
-        self.dam = response['daily'];
-        self.dam_list = response['daily'].slice(0).reverse();
-        self.mam = response['monthly'];
-        self.mam_list = response['monthly'].slice(0).reverse();
-      });
-  }
+    /**
+     * Return active user analytics
+     */
+    getActives() {
+        var self = this;
+        this.client.get('api/v1/admin/analytics/active')
+            .then((response: any) => {
+                self.dam = response['daily'];
+                self.dam_list = response['daily'].slice(0).reverse();
+                self.mam = response['monthly'];
+                self.mam_list = response['monthly'].slice(0).reverse();
+            });
+    }
 
-  /**
-   * Return signups
-   */
-  getSignups() {
-    this.client.get('api/v1/admin/analytics/signups')
-      .then((response: any) => {
-        this.signups = response['daily'];
-        this.signups_list = response['daily'].slice(0).reverse();
-      });
-  }
+    /**
+     * Return pageviews
+     */
+    getPageviews() {
+        this.client.get('api/v1/admin/analytics/pageviews')
+            .then((response: any) => {
+                this.pageviews = response['pageviews'];
+                this.pageviews_list = response['pageviews'].slice(0).reverse();
+            });
+    }
 
-  /**
-   * Return retention rates
-   */
-  getRetention() {
-    this.client.get('api/v1/admin/analytics/retention')
-      .then((response: any) => {
-        this.retention = response.retention[0];
-        console.log(this.retention);
-      });
-  }
+    /**
+     * Return signups
+     */
+    getSignups() {
+        this.client.get('api/v1/admin/analytics/signups')
+            .then((response: any) => {
+                this.signups = response['daily'];
+                this.signups_list = response['daily'].slice(0).reverse();
+            });
+    }
 
-  /**
-   * Return boost analytics
-   */
-  getBoosts() {
-    var self = this;
-    this.client.get('api/v1/admin/analytics/boost')
-      .then((response: any) => {
-        self.boost_newsfeed = response.newsfeed;
-        self.boost_newsfeed.total = self.boost_newsfeed.review + self.boost_newsfeed.approved;
-        self.boost_newsfeed.percent = (self.boost_newsfeed.approved / self.boost_newsfeed.total) * 100;
-      });
-  }
+    /**
+     * Return retention rates
+     */
+    getRetention() {
+        this.client.get('api/v1/admin/analytics/retention')
+            .then((response: any) => {
+                this.retention = response.retention[0];
+                console.log(this.retention);
+            });
+    }
+
+    /**
+     * Return boost analytics
+     */
+    getBoosts() {
+        var self = this;
+        this.client.get('api/v1/admin/analytics/boost')
+            .then((response: any) => {
+                self.boost_newsfeed = response.newsfeed;
+                self.boost_newsfeed.total = self.boost_newsfeed.review + self.boost_newsfeed.approved;
+                self.boost_newsfeed.percent = (self.boost_newsfeed.approved / self.boost_newsfeed.total) * 100;
+            });
+    }
 
 }
--- app/src/controllers/admin/featured/featured.ts
+++ app/src/controllers/admin/featured/featured.ts
@@ -3,78 +3,78 @@ import { Component } from '@angular/core';
 import { Client } from '../../../services/api';
 
 @Component({
-  moduleId: module.id,
-  selector: 'minds-admin-featured',
-  templateUrl: 'featured.html',
+    moduleId: module.id,
+    selector: 'minds-admin-featured',
+    templateUrl: 'featured.html',
 })
 
 export class AdminFeatured {
 
-  categories: any[] | null;
-  category: string = '';
-  featured: any[] = [];
+    categories: any[] | null;
+    category: string = '';
+    featured: any[] = [];
 
-  inProgress: boolean = false;
-  moreData: boolean = true;
+    inProgress: boolean = false;
+    moreData: boolean = true;
 
-  constructor(public client: Client) {
-  }
-
-  ngOnInit() {
-    this.loadCategories(window.Minds.categories);
-  }
-
-  load(refresh: boolean = false) {
-    if (this.inProgress) {
-      return;
+    constructor(public client: Client) {
     }
 
-    if (!this.category) {
-      this.moreData = false;
-      return;
+    ngOnInit() {
+        this.loadCategories(window.Minds.categories);
     }
 
-    this.inProgress = true;
-
-    if (refresh) {
-      this.featured = [];
-      this.moreData = true;
-    }
-
-    this.client.get(`api/v1/categories/featured`, {
-      categories: [this.category]
-    })
-      .then((response: any) => {
-        //@todo: refactor if pagination (offset) is implemented
-        this.moreData = false;
-        this.inProgress = false;
-
-        if (!response.entities) {
-          this.inProgress = false;
-          return;
+    load(refresh: boolean = false) {
+        if (this.inProgress) {
+            return;
         }
 
-        this.featured.push(...response.entities);
-      })
-      .catch(e => {
-        this.moreData = false;
-        this.inProgress = false;
-      });
-  }
+        if (!this.category) {
+            this.moreData = false;
+            return;
+        }
 
-  setCategory(category: string) {
-    this.category = category;
-    this.load(true);
-  }
+        this.inProgress = true;
 
-  loadCategories(categories: any) {
-    this.categories = [];
+        if (refresh) {
+            this.featured = [];
+            this.moreData = true;
+        }
 
-    for (let key in categories) {
-      this.categories.push({
-        key,
-        value: categories[key],
-      });
+        this.client.get(`api/v1/categories/featured`, {
+            categories: [this.category]
+        })
+            .then((response: any) => {
+                //@todo: refactor if pagination (offset) is implemented
+                this.moreData = false;
+                this.inProgress = false;
+
+                if (!response.entities) {
+                    this.inProgress = false;
+                    return;
+                }
+
+                this.featured.push(...response.entities);
+            })
+            .catch(e => {
+                this.moreData = false;
+                this.inProgress = false;
+            });
     }
-  }
+
+    setCategory(category: string) {
+        this.category = category;
+        this.load(true);
+    }
+
+    loadCategories(categories: any) {
+        this.categories = [];
+
+        for (let category in categories) {
+            this.categories.push({
+                id: category,
+                value: window.Minds.categories[category],
+            });
+        }
+    }
 }
